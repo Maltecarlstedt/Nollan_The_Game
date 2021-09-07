45a1b22fa8cb25519cd259227ab965edc0c9b4f6
maltecarlstedt <malte.carlstedt@hotmail.se>
2021-09-07 14:39:13 +0200

    Initial Gamefiles Created

---
+++ src/main/java/SetupClass.java
@@ -0,0 +1,34 @@
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class SetupClass extends StateBasedGame {
+    public static final int canvasWidth = 1024;
+    public static final int canvasHeight = 768;
+
+    public SetupClass(String title) {
+        super(title);
+    }
+
+    @Override
+    public void initStatesList(GameContainer gameContainer) throws SlickException {
+        this.addState(new GameState());
+
+    }
+
+
+
+
+    public static void main(String[] args) throws SlickException {
+        AppGameContainer app = new AppGameContainer(new SetupClass("Nollans Första Dag"));
+
+        app.setIcons(new String[]{"data/icon32.png", "data/icon16.png"});
+
+        app.setTargetFrameRate(60);
+
+
+        app.setShowFPS(false);
+
+        app.setDisplayMode(canvasWidth,canvasHeight,false);
+        app.start();
+    }
+}
