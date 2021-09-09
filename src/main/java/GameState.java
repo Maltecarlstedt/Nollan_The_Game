af192dc8e5b725ce73b475dc64f43cb09262fe15
alexanderbrunnegard <alex_brunnegard98@hotmail.com>
2021-09-09 12:58:55 +0200

    Added JavaDoc

--- src/main/java/GameState.java
+++ src/main/java/GameState.java
@@ -2,24 +2,37 @@ import org.newdawn.slick.*;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
-public class GameState extends BasicGameState {
+public class GameState extends BasicGameState { //TODO: Ska detta verkligen extenda BasicGameState?
 
-
-
+    /**
+     * Declares our map variable as well as our player.
+     */
     private Image map;
 
     Player player = new Player();
 
 
-
+    /**
+     * Initialise this state. It should load any resources it needs at this stage, in our case our map and our player
+     * @param gameContainer The container holding the game
+     * @param stateBasedGame The game holding this state
+     * @throws SlickException creates an exception if the map file is not found
+     */
     @Override
     public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
         map = new Image("data/maps/testMap.png");
         player.initPlayer();
-
-
     }
 
+    /**
+     * Update the state's logic based on the amount of time that has passed.
+     * In our update we check the keypresses and updates the game accordingly
+     * Updates the animation based on the time that has passed since the last update.
+     * @param gameContainer The container holding the game
+     * @param stateBasedGame The game holding this state
+     * @param delta The amount of time that has passed in millisecond since last update
+     * @throws SlickException creates an exception if the map file is not found
+     */
     @Override
     public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int delta) throws SlickException {
         player.MoveRightAni.update(delta); // this line makes sure the speed of the Animation is true
@@ -51,11 +64,21 @@ public class GameState extends BasicGameState {
     }
 
 
-
+    /**
+     * Render this state to the game's graphics context, in our program that is the background image and the player
+     * @param gameContainer The container holding the game
+     * @param stateBasedGame The game holding this state
+     * @param graphics The graphics context to render to
+     * @throws SlickException creates an exception if the map or player image file is not found
+     */
     @Override
     public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
 
         map.draw(0,0,SetupClass.canvasWidth, SetupClass.canvasHeight);
+
+        //TODO: borde använda graphics.drawImage(player.getCurrentImage, player.getLocation().x, player.getLocation().y)??
+        //TODO: Samma med graphics.drawAnimation(...) som ^^^^?
+
 
         if(player.getOrientation() == Orientation.IDLE){//TODO: Ingen logik i render
             player.currentImage.draw(player.getLocation().x, player.getLocation().y,player.getWidth(),player.getHeight());
@@ -66,7 +89,9 @@ public class GameState extends BasicGameState {
     }
 
 
-
+    /**
+     * @return the ID of this game state
+     */
     @Override
     public int getID() {
         return 0;
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -2,10 +2,19 @@ import org.newdawn.slick.Animation;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.SpriteSheet;
+import org.newdawn.slick.tiled.TiledMap;
 
 import java.awt.*;
 
 public class Player {
+
+    /**
+     * Creates variables for a player object
+     * Spritesheets are Images that we split up to create our animations of walking
+     * currentImage has the current image when the player is IDLE
+     * currentAnimation is an animation that changes depending on which orientation the player has
+     * State gives the state that the player is in. I.E which way the player is facing
+     */
     private Point location;
     private int width, height;
     private Orientation orientation;
@@ -28,14 +37,20 @@ public class Player {
 
     private int state = 0;
 
+
+    /**
+     * Creates an idle, basic player in the center of our window
+     */
     public Player(){
         location = new Point(1024/2, 768/2);
         width = 64;
         height = 64;
         orientation = Orientation.IDLE;
-
     }
 
+    /**
+     * Sets up our animation that we use for walking in game
+     */
     public void animationSetup(){
         MoveDownAni = new Animation();
         MoveLeftAni = new Animation();
@@ -56,7 +71,12 @@ public class Player {
         }
     }
 
-    public void spriteSetup() throws SlickException {
+    /**
+     * Creates all of our sprites, which basically is an image that we split up into 4 different subimages.
+     * Reason being that we use these 4 subimages to create an animation.
+     * @throws SlickException throws an exception if the file for the SpriteSheet is not found.
+     */
+    public void spriteSetup() throws SlickException {//TODO: Borde skala vår gubbe här direkt till 64x64?
         MoveRight = new SpriteSheet("data/playerAnimation/gubbeRIGHT.spritesheet.png",1841/4,600); // declare a SpriteSheet and load it into java with its dimentions
         MoveLeft = new SpriteSheet("data/playerAnimation/gubbeLEFT.spritesheet.png",1841/4,600); // declare a SpriteSheet and load it into java with its dimentions
         MoveUp = new SpriteSheet("data/playerAnimation/gubbeUP.spritesheet.png",1841/4,600); // declare a SpriteSheet and load it into java with its dimentions
@@ -64,6 +84,10 @@ public class Player {
 
     }
 
+    /**
+     * Initiates all the attributes for our player.
+     * @throws SlickException throws an exception if the file for the SpriteSheet is not found.
+     */
     public void initPlayer() throws SlickException {
         spriteSetup();
         animationSetup();
@@ -72,40 +96,73 @@ public class Player {
 
     }
 
+    /**
+     * @return player's height
+     */
     public int getHeight() {
         return height;
     }
 
+    /**
+     * @return player's width
+     */
     public int getWidth() {
         return width;
     }
 
+    /**
+     * @return player's location
+     */
     public Point getLocation() {
         return location;
     }
 
+    /**
+     * Set player's coordinates
+     * @param x player's x coordinate
+     * @param y player's y coordinate
+     */
     public void setLocation(int x, int y) {
         location.x = x;
         location.y = y;
     }
 
+    /**
+     * Set player's orientation
+     * @param orientation player's orientation
+     */
     public void setOrientation(Orientation orientation) {
         this.orientation = orientation;
     }
 
+    /**
+     * @return player's orientation as the enum we created
+     */
     public Orientation getOrientation() {
         return orientation;
     }
 
+    /**
+     * set player's location
+     * @param location player's location as a Point
+     */
     public void setLocation(Point location) {
         this.location = location;
     }
 
+    /**
+     * Moves our player depending on the orientation. Our delta-values changes depending on the orientation
+     * (check the Orientation enum for more precise explanation)
+     */
     public void move() {
         location.x += orientation.deltaX * 2;
         location.y += orientation.deltaY * 2;
     }
 
+    /**
+     * Moves the character up, changes state so that if the player is IDLE, the player will use the right IDLE state
+     * (I.E it will stand still in state UP)
+     */
     public void moveUp() {
         setOrientation(Orientation.UP);
         move();
@@ -113,6 +170,9 @@ public class Player {
         state = 3;
     }
 
+    /**
+     * Moves the character to the left, changes state so that if the player is IDLE, the player will use the right IDLE state
+     */
     public void moveLeft() {
         setOrientation(Orientation.LEFT);
         move();
@@ -120,6 +180,9 @@ public class Player {
         state = 2;
     }
 
+    /**
+     * Moves the character to the right, changes state so that if the player is IDLE, the player will use the right IDLE state
+     */
     public void moveRight() {
         setOrientation(Orientation.RIGHT);
         move();
@@ -127,6 +190,9 @@ public class Player {
         state = 1;
     }
 
+    /**
+     * Moves the character down, changes state so that if the player is IDLE, the player will use the right IDLE state
+     */
     public void moveDown() {
         setOrientation(Orientation.DOWN);
         move();
@@ -134,6 +200,10 @@ public class Player {
         state = 0;
     }
 
+    /**
+     * Sets the still image so that the player just stops in its track. Depending on which state is set,
+     * which is given by which key was most lately pressed down.
+     */
     public void idlePlayer() {
 
         switch (state){
