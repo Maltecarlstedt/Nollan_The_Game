14b48206fc4f39a12b854267df2f5c1e05fdaba5
alexanderbrunnegard <alex_brunnegard98@hotmail.com>
2021-09-13 11:12:42 +0200

    Refactored the movement constraints, tidied up some redundant code

--- src/main/java/Orientation.java
+++ src/main/java/Orientation.java
@@ -4,7 +4,7 @@
  */
 
 public enum Orientation {
-    RIGHT(1,0), UP(0,-1), LEFT(-1,0), DOWN(0,1), IDLE(0,0);
+    RIGHT(2,0), UP(0,-2), LEFT(-2,0), DOWN(0,2), IDLE(0,0);
     public int deltaX, deltaY;
 
     Orientation(int deltaX, int deltaY) {
