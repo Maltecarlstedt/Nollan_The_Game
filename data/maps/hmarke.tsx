075fab313ed6548284ad692d5459def39027f8ac
steffi0929 <steffi.gumman@hotmail.com>
2021-09-16 15:28:51 +0200

    small fixes with the documents

--- data/maps/hmarke.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="hmarke" tilewidth="32" tileheight="32" tilecount="9" columns="3">
- <image source="../../../../../../Downloads/hmarke96.png" width="96" height="96"/>
-</tileset>
