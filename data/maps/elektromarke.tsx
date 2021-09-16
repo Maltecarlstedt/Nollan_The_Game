ff10364e470283fd4e3a07a83527c3b0cd6decf7
steffi0929 <steffi.gumman@hotmail.com>
2021-09-16 15:15:33 +0200

    fixed aDammen

---
+++ data/maps/elektromarke.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="elektromarke" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="../../../../../../Downloads/elektromarke96.png" width="96" height="96"/>
+</tileset>
---
+++ data/maps/startText.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="startText" tilewidth="32" tileheight="32" tilecount="768" columns="32">
+ <image source="../../../../../../Downloads/Nytt projekt.png" width="1024" height="768"/>
+</tileset>
