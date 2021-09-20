a525f535a3b403afac7f68a3dfe257633a47c1ad
steffi0929 <steffi.gumman@hotmail.com>
2021-09-20 16:23:01 +0200

    added some maps and fixed the startscreen

--- data/maps/tsx-files/colstuff.tsx
+++ data/maps/tsx-files/colstuff.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="colstuff" tilewidth="32" tileheight="32" tilecount="1064" columns="8">
- <image source="images/[Base]BaseChip_pipo.png" width="256" height="4256"/>
+<tileset version="1.5" tiledversion="1.7.2" name="colstuff" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="../images/base_out_atlas.png" width="1024" height="1024"/>
 </tileset>
