31245f9b232bc518f16130cf6e95e8ed39abe27b
ClaraSimonsson <clara.simonsson@hotmail.se>
2021-09-20 09:00:23 +0200

    Added comments for the map-related classes, interfaces and enums

--- src/main/java/Chalmersplatsen.java
+++ src/main/java/Chalmersplatsen.java
@@ -1,19 +1,30 @@
 import org.newdawn.slick.*;
 import org.newdawn.slick.tiled.TiledMap;
 
+/**
+ * Creates an enum for one map piece which holds the specific methods for just that map.
+ * This map is for the tiled map Chalmershållplatsen.
+ */
 public enum Chalmersplatsen implements MapState {
     CHALMERSPLATSEN;
+
     @Override
     public void nextState(Map input, Player player){
         switch (input.getPOrientation(player)) {
-            case LEFT -> input.setTiledMap(Chalmersplatsen.CHALMERSPLATSEN);
-            case DOWN -> input.setTiledMap(Karhuset.KARHUSET);
+            case LEFT:
+                input.setTiledMap(Chalmersplatsen.CHALMERSPLATSEN);
+                player.setLocation(650,650);
+                break;
+            case UP:
+                input.setTiledMap(Karhuset.KARHUSET);
+                player.setLocation(350,700);
+                break;
         }
     }
 
     @Override
     public void setPosition(Player player) {
-        player.setLocation(450,500);
+        player.setLocation(200,75);
     }
 
     @Override
--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -5,32 +5,63 @@ import org.newdawn.slick.tiled.TiledMap;
 
 import java.awt.*;
 
+/**
+ * Have the information about which map the Player is in and where it should go next.
+ */
 public class Map {
+    /**
+     * MapState current knows which map the player is in.
+     * TiledMap tiledMap makes it possible to show the map on the screen
+     */
     private MapState current;
     public TiledMap tiledMap;
 
+    /**
+     * Sets the map to the MapState Kårhuset, which is the one the player will start in.
+     */
     public Map(){
         current = Karhuset.KARHUSET;
     }
 
-    //When the game starts it will start in the map Kårhuset
+    /**
+     * Loads the map for Kårhuset
+     * @throws SlickException throws an exception if the file for the TiledMap is not found.
+     */
     public void initMap() throws SlickException {tiledMap = current.loadMap();}
 
+    /**
+     * Sets the tiled map to the current state map.
+     * @param current the current state
+     */
     public void setTiledMap(MapState current){this.current = current;}
 
+    /**
+     *
+     * @return the current state
+     */
     public MapState getCurrentState(){return current;}
 
+    /**
+     * Checks if the player has gone outside the frame and should be moved to a new map
+     * and the state would be updated.
+     * @param player check if its ouside the frame.
+     * @throws SlickException throws an exception if the file for the TiledMap is not found.
+     */
     public void isOutside(Player player) throws SlickException {
         if (!player.checkBorder()) {
             current.nextState(this, player);
-            current.setPosition(player);
+            //current.setPosition(player);
             tiledMap = current.loadMap();
         }
     }
 
+    /**
+     * Gets the orientation of the player to check in which direction
+     * the player had walked outside the frame.
+     * @param player gets the direction of the player
+     * @return the direction of the player
+     */
     public Orientation getPOrientation(Player player){
         return player.getOrientation();
     }
-
-
 }
