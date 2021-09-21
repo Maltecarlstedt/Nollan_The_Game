aa70a701cfa8808e52ddcc9d9bde8c5c330f8eea
steffi0929 <steffi.gumman@hotmail.com>
2021-09-21 15:32:46 +0200

    more fixes for the maps

---
+++ data/maps/tsx-files/pool_cookout.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="pool_cookout" tilewidth="32" tileheight="32" tilecount="54" columns="9">
+ <image source="../images/pool_cookout.png" width="288" height="192"/>
+</tileset>
