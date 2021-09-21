d75be6172f4062e7f793cac79bb2a31a0ec6b534
maltecarlstedt <malte.carlstedt@hotmail.se>
2021-09-21 15:24:42 +0200

    Starting to reimplement the beerchugging task after the refactoring

---
+++ src/main/java/Tasks/TasksManager.java
@@ -0,0 +1,4 @@
+package Tasks;
+
+public class TasksManager {
+}
---
+++ src/main/java/Tasks/taskView/chuggingIndicatorView.java
@@ -0,0 +1,14 @@
+package Tasks.taskView;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+
+public class chuggingIndicatorView {
+
+
+
+    public void render(GameContainer gc, Graphics g){
+
+
+    }
+}
