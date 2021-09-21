44d1e7e17488f5ba73eee824d138e7179271b16c
steffi0929 <steffi.gumman@hotmail.com>
2021-09-21 11:58:17 +0200

    Refactored the maps and images

---
+++ data/maps/tsx-files/moreHouses.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="moreHouses" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="images/moreHouses.jpg" width="512" height="512"/>
+</tileset>
