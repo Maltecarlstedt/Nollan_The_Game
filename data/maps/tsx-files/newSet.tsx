89a45443873d76a1624fab41aa569b4f13e3469b
steffi0929 <steffi.gumman@hotmail.com>
2021-09-21 12:07:17 +0200

    fixed the txs files

--- data/maps/tsx-files/newSet.tsx
+++ data/maps/tsx-files/newSet.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="newSet" tilewidth="32" tileheight="32" tilecount="180" columns="20">
- <image source="../images/RPG Nature Tileset.png" width="641" height="288"/>
+ <image source="images/RPG Nature Tileset.png" width="641" height="288"/>
 </tileset>
