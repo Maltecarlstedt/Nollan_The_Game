cb78a9d96d58c8de64f2dfaddd50ce2cd43ff9cd
steffi0929 <steffi.gumman@hotmail.com>
2021-09-21 17:02:12 +0200

    fixed a beginning of the new starter menu

--- src/main/java/GameMenu.java
+++ src/main/java/GameMenu.java
@@ -3,24 +3,53 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+import org.lwjgl.input.Mouse;
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
 
 
 public class GameMenu extends BasicGameState {
 
+    Image startGame;
+    Image exitGame;
+
+    /*
+    public Menu(int state) {
+
+    }
+*/
 
     @Override
-    public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
+    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+        startGame = new Image("data/maps/images/startaSpeletButton.png");
+        exitGame = new Image("data/maps/images/avslutaButton.png");
+    }
+
+    @Override
+    public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+        startGame.drawCentered(1024/2,768/2);
+        exitGame.drawCentered(1024/2,500);
 
     }
 
     @Override
-    public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
+    public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
+        int posX = Mouse.getX();
+        int posY = Mouse.getY();
 
-    }
+        // start game button
+        if ((posX > 243 && posX < 781) && (posY > 337 && posY < 431)) {
+            if (Mouse.isButtonDown(0)) {
+                sbg.enterState(1);
+            }
+        }
 
-    @Override
-    public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
-
+        // exit game button
+        if ((posX > 243 && posX < 781) && (posY > 221 && posY < 315)) {
+            if (Mouse.isButtonDown(0)) {
+                System.exit(0);
+            }
+        }
     }
 
     @Override
