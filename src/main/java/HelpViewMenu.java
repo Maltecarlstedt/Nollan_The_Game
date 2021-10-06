2beae3b7ac8d3fcbffcaf5fdbd4d9d313df113ca
steffi0929 <steffi.gumman@hotmail.com>
2021-10-06 15:06:57 +0200

    added javadoc

--- src/main/java/HelpViewMenu.java
+++ src/main/java/HelpViewMenu.java
@@ -8,42 +8,68 @@ import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.state.transition.FadeInTransition;
 import org.newdawn.slick.state.transition.FadeOutTransition;
 
+/** Represents the help view menu.
+ */
 public class HelpViewMenu extends BasicGameState {
 
+    /** Some images representing "buttons".
+     */
     Image bg;
     Image infoText;
     Image startGame;
     // Image backButton;
 
+
+    /** Initiating the images.
+     * @param gc represents the container that have the game.
+     * @param sbg the current state of the game used to isolate the game from different aspects.
+     * @throws SlickException SlickException if file not found, slick-exception.
+     */
     @Override
-    public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
+    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
         bg = new Image("data/maps/images/startscreen.png");
         infoText = new Image("data/maps/images/helpText.png");
         startGame = new Image("data/maps/images/borjaSpela.png");
         //backButton = new Image("data/maps/images/backButton.png");
     }
 
+    /** Rendering all the images.
+     * @param gc represents the container that have the game.
+     * @param sbg the current state of the game used to isolate the game from different aspects.
+     * @param g represents the graphics context to be used for rendering.
+     * @throws SlickException SlickException if file not found, slick-exception.
+     */
     @Override
-    public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
+    public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
         bg.draw();
         infoText.drawCentered(1024/2,768/2);
         startGame.drawCentered(1024/2,616);
         //backButton.drawCentered(1024/2,616);
     }
 
+    /** Checks the mouse, and enters a state if "if-statement" is fulfilled.
+     * @param gc represents the container that have the game.
+     * @param sbg the current state of the game used to isolate the game from different aspects.
+     * @param delta represents time in ms since last update.
+     * @throws SlickException SlickException if file not found, slick-exception.
+     */
     @Override
     public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
         int posX = Mouse.getX();
         int posY = Mouse.getY();
 
-        // back to the menu button
+        // start the game button
         if ((posX > 243 && posX < 781) && (posY > 106 && posY < 194)) {
             if (Mouse.isButtonDown(0)) {
                 sbg.enterState(1, new FadeOutTransition(), new FadeInTransition());
             }
         }
+        // TODO: fix a back "button" to get to the main menu
     }
 
+    /** A special ID for this specific state.
+     * @return represents this state with a unique int.
+     */
     @Override
     public int getID() {
         return 5;
--- src/main/java/Tasks/taskModel/GatheringPantModel.java
+++ src/main/java/Tasks/taskModel/GatheringPantModel.java
@@ -6,25 +6,35 @@ import org.newdawn.slick.SlickException;
 
 import java.util.ArrayList;
 
+/** Model of the task "Gathering pant".
+ * @auther Steffanie Kristiansson
+ */
 public class GatheringPantModel {
 
-    /**
-     * GatheringPantModel should handle the logic behind the "task":
-     *          - Keep track of score and time
-     *          -
-     *          -
+    /** Represents time passed on pant-task.
      */
-
     public float pantTimePassed;
 
+
+    /** An ArrayList of all the pant.
+     */
     ArrayList<Pant> pants = new ArrayList<>();
 
 
+    /** Gets the list of all the pant.
+     * @return An array-list of all the pant.
+     */
     public ArrayList<Pant> getPants() {
         return pants;
     }
 
+
+    /** Creates a new pant.
+     * @throws SlickException if file not found, slick-exception.
+     */
     public void addPant() throws SlickException {
         pants.add(new Pant());
     }
+
+    // TODO: create some kind of database (or such) to save the score for each completed task.
 }
