7ae2b397c91ecc168a0d9f741d99c363c336eab9
ClaraSimonsson <clara.simonsson@hotmail.se>
2021-10-11 10:10:41 +0200

    Added unfilled icons and implemented them in the game

--- src/main/java/model/MaterialModel.java
+++ src/main/java/model/MaterialModel.java
@@ -12,23 +12,56 @@ public class MaterialModel {
     public Image scissor;
     private Point scissorLocation;
 
-    public Image scissorUnfilled;
-    private Point scissorUnfilledLocation;
+    public Image scissorUf;
+    private Point scissorUfLocation;
 
     public Image karkort;
     private Point karkortLocation;
 
+    public Image karkortUf;
+    private Point karkortUfLocation;
+
     public Image cardboard;
     private Point cardboardLocation;
+
+    public Image cardboardUf;
+    private Point cardboardUfLocation;
+
+    public Image rope;
+    private Point ropeLocation;
+
+    public Image ropeUf;
+    private Point ropeUfLocation;
+
+    public Image blackColor;
+    private Point blackColorLocation;
+
+    public Image blackColorUf;
+    private Point blackColorUfLocation;
+
+    public Image turqoiseColor;
+    private Point turqoiseColorLocation;
+
+    public Image turqoiseColorUf;
+    private Point turqoiseColorUfLocation;
 
     public Rectangle rect;
 
     public MaterialModel() throws SlickException {
+        initScissorUf();
+        initKarkortUf();
+        initCardboardUf();
+        initRopeUf();
+        initBlackColorUf();
+        initTurqoiseColorUf();
+
         initScissor();
         initRect();
-        initScissorUnfilled();
         initKarkort();
         initCardboard();
+        initRope();
+        initBlackColor();
+        initTurqoiseColor();
     }
 
     public void initScissor() throws SlickException {
@@ -42,29 +75,101 @@ public class MaterialModel {
 
     public void initKarkort() throws SlickException {
         karkort = new Image("data/material/karkortet.png");
-        karkortLocation = new Point(getStartX()+40,getStartY());
+        karkortLocation = new Point(300, 300);
     }
 
-    public Point getkarkortLocation(){
+    public Point getKarkortLocation(){
         return karkortLocation;
     }
 
     public void initCardboard() throws SlickException {
         cardboard = new Image("data/material/cardboard.png");
-        cardboardLocation = new Point(getStartX()+80,getStartY());
+        cardboardLocation = new Point(350, 350);
     }
 
     public Point getCardboardLocation(){
         return cardboardLocation;
     }
 
-    public void initScissorUnfilled() throws SlickException {
-        scissorUnfilled = new Image("data/material/scissorUnfilled.png");
-        scissorUnfilledLocation = new Point(getStartX(), getStartY());
+    public void initRope() throws SlickException {
+        rope = new Image("data/material/rope.png");
+        ropeLocation = new Point(400, 400);
     }
 
-    public Point getScissorUnfilledLocation(){
-        return scissorUnfilledLocation;
+    public Point getRopeLocation(){
+        return ropeLocation;
+    }
+
+    public void initBlackColor() throws SlickException {
+        blackColor = new Image("data/material/black_color.png");
+        blackColorLocation = new Point(450, 450);
+    }
+
+    public Point getBlackColorLocation(){
+        return blackColorLocation;
+    }
+
+    public void initTurqoiseColor() throws SlickException {
+        turqoiseColor = new Image("data/material/turqoise_color.png");
+        turqoiseColorLocation = new Point(500, 500);
+    }
+
+    public Point getTurqoiseColorLocation(){
+        return turqoiseColorLocation;
+    }
+
+    public void initScissorUf() throws SlickException {
+        scissorUf = new Image("data/material/scissor_uf.png");
+        scissorUfLocation = new Point(getStartX(), getStartY());
+    }
+
+    public Point getScissorUfLocation(){
+        return scissorUfLocation;
+    }
+
+    public void initKarkortUf() throws SlickException {
+        karkortUf = new Image("data/material/karkortet_uf.png");
+        karkortUfLocation = new Point(getStartX()+50,getStartY());
+    }
+
+    public Point getKarkortUfLocation(){
+        return karkortUfLocation;
+    }
+
+    public void initCardboardUf() throws SlickException {
+        cardboardUf = new Image("data/material/cardboard_uf.png");
+        cardboardUfLocation = new Point(getStartX()+100,getStartY());
+    }
+
+    public Point getCardboardUfLocation(){
+        return cardboardUfLocation;
+    }
+
+    public void initRopeUf() throws SlickException {
+        ropeUf = new Image("data/material/rope_uf.png");
+        ropeUfLocation = new Point(getStartX()+150,getStartY());
+    }
+
+    public Point getRopeUfLocation(){
+        return ropeUfLocation;
+    }
+
+    public void initBlackColorUf() throws SlickException {
+        blackColorUf = new Image("data/material/black_color_uf.png");
+        blackColorUfLocation = new Point(getStartX()+200,getStartY());
+    }
+
+    public Point getBlackColorUfLocation(){
+        return blackColorUfLocation;
+    }
+
+    public void initTurqoiseColorUf() throws SlickException {
+        turqoiseColorUf = new Image("data/material/turqoise_color_uf.png");
+        turqoiseColorUfLocation = new Point(getStartX()+250,getStartY());
+    }
+
+    public Point getTurqoiseColorUfLocation(){
+        return turqoiseColorUfLocation;
     }
 
     public void initRect(){
@@ -72,7 +177,7 @@ public class MaterialModel {
     }
 
     private int getStartX(){
-       return 710;
+       return 720;
     }
     private int getStartY(){
         return 725;
--- src/main/java/view/MaterialView.java
+++ src/main/java/view/MaterialView.java
@@ -14,9 +14,12 @@ public class MaterialView {
 
     public void renderMaterial(Graphics g, MaterialModel materialModel){
         g.fill(materialModel.rect);
-        g.drawImage(materialModel.scissorUnfilled, materialModel.getScissorUnfilledLocation().x, materialModel.getScissorUnfilledLocation().y);
-        g.drawImage(materialModel.karkort, materialModel.getkarkortLocation().x, materialModel.getkarkortLocation().y);
-        g.drawImage(materialModel.cardboard, materialModel.getCardboardLocation().x, materialModel.getCardboardLocation().y);
+        g.drawImage(materialModel.scissorUf, materialModel.getScissorUfLocation().x, materialModel.getScissorUfLocation().y);
+        g.drawImage(materialModel.karkortUf, materialModel.getKarkortUfLocation().x, materialModel.getKarkortUfLocation().y);
+        g.drawImage(materialModel.cardboardUf, materialModel.getCardboardUfLocation().x, materialModel.getCardboardUfLocation().y);
+        g.drawImage(materialModel.ropeUf, materialModel.getRopeUfLocation().x, materialModel.getRopeUfLocation().y);
+        g.drawImage(materialModel.blackColorUf, materialModel.getBlackColorUfLocation().x, materialModel.getBlackColorUfLocation().y);
+        g.drawImage(materialModel.turqoiseColorUf, materialModel.getTurqoiseColorUfLocation().x, materialModel.getTurqoiseColorUfLocation().y);
     }
 
     public void renderFindMaterial(Graphics g, MaterialModel materialModel, MapModel mapModel){
@@ -26,8 +29,13 @@ public class MaterialView {
 
     public void drawMaterial(MapModel mapModel, Graphics g, MaterialModel materialModel){
         currentMap = mapModel.getCurrentMap().toString();
-        if (currentMap.equals("ADAMMEN")){
+        if (currentMap.equals("KARHUSET")){
             g.drawImage(materialModel.scissor, materialModel.getScissorLocation().x, materialModel.getScissorLocation().y);
+            g.drawImage(materialModel.karkort, materialModel.getKarkortLocation().x, materialModel.getKarkortLocation().y);
+            g.drawImage(materialModel.cardboard, materialModel.getCardboardLocation().x, materialModel.getCardboardLocation().y);
+            g.drawImage(materialModel.rope, materialModel.getRopeLocation().x, materialModel.getRopeLocation().y);
+            g.drawImage(materialModel.blackColor, materialModel.getBlackColorLocation().x, materialModel.getBlackColorLocation().y);
+            g.drawImage(materialModel.turqoiseColor, materialModel.getTurqoiseColorLocation().x, materialModel.getTurqoiseColorLocation().y);
         }
     }
 }
