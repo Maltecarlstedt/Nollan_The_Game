331dadaf88bf4b57415e8d3ab9d1f2c044aa0d04
ClaraSimonsson <clara.simonsson@hotmail.se>
2021-10-15 10:51:48 +0200

    Started with tests for mapModel

---
+++ test/testMap.java
@@ -0,0 +1,32 @@
+import model.CollisionChecker;
+import model.MapModel;
+import model.MapStates.Ekak;
+import model.MapStates.Karhuset;
+import org.junit.Before;
+import org.junit.Test;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.tiled.TiledMap;
+
+import static org.junit.Assert.*;
+
+public class testMap {
+    MapModel mapModel;
+
+    CollisionChecker collisionChecker;
+
+    @Before
+    public void init() throws SlickException {
+        collisionChecker = new CollisionChecker();
+        mapModel = new MapModel(collisionChecker);
+    }
+
+    /*
+    @Test
+    public void testSetMap(){
+        assertEquals(mapModel.getCurrentMap(), Karhuset.KARHUSET);
+    }
+
+     */
+
+
+}
