4bbe14ee0f4b7501fe5366b27a135dfa4447f5dd
ClaraSimonsson <clara.simonsson@hotmail.se>
2021-10-15 11:33:02 +0200

    The javadocs for the Item package is complete

--- src/main/java/Items/ItemModel.java
+++ src/main/java/Items/ItemModel.java
@@ -9,8 +9,15 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * Holds the information for the items to be found and the unfilled items
+ */
 public class ItemModel {
 
+    /**
+     * All items to be found and all unfilled items with their specific characteristics.
+     * The hashmaps/lists with the items
+     */
     public Rectangle rect;
 
     private Item scissor = new Scissor();
@@ -31,10 +38,10 @@ public class ItemModel {
     private HashMap<String, Item> materialsUnfilled = new HashMap<>();
    // private ArrayList<MapState> itemMaps = new ArrayList<>();
 
-    public HashMap<String, Item> getItemsToFind(){ return materialsFilled;}
-    public HashMap<String, Item> getItemsUf(){ return materialsUnfilled;}
-
-
+    /**
+     * The constructor of the ItemModel. It puts all the items in the lists
+     * @throws SlickException - throws an exception if a filepath to the image or map is not found
+     */
     public ItemModel() throws SlickException {
         //initModel();
 
@@ -55,6 +62,18 @@ public class ItemModel {
 
 
     }
+
+    /**
+     * Gets the list with the items that needs to be found.
+     * @return the list of items to be found
+     */
+    public HashMap<String, Item> getItemsToFind(){ return materialsFilled;}
+
+    /**
+     * Gets the list of items that are not found yet
+     * @return the list of unfilled items
+     */
+    public HashMap<String, Item> getItemsUf(){ return materialsUnfilled;}
     /*
     public void initModel(){
         itemMaps.add(scissor.getCurrentMap());
@@ -67,6 +86,10 @@ public class ItemModel {
 
      */
 
+    /**
+     * Checks if all items has been found
+     * @return true if the list of items to be found is empty
+     */
     public boolean itemsCollected(){
         if (getItemsToFind().size() == 0){
             System.out.println("Spelet är slut");
@@ -75,6 +98,9 @@ public class ItemModel {
         return false;
     }
 
+    /**
+     * Creates a rectangle in the corner to make it easier to see which items to be collected
+     */
     public void initRect(){
         rect = new Rectangle(700,710, 350, 70);
     }
