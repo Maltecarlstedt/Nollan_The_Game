2fde9b67506f975b8ea60e6ddced6d9f57186112
ClaraSimonsson <clara.simonsson@hotmail.se>
2021-10-15 11:42:00 +0200

    Javadocs in GameDoneView

--- src/main/java/GameDoneView.java
+++ src/main/java/GameDoneView.java
@@ -14,8 +14,14 @@ import org.newdawn.slick.tiled.TiledMap;
 import view.MapView;
 import view.PlayerView;
 
+/**
+ * Represents the view when the game has ended
+ */
 public class GameDoneView extends BasicGameState {
 
+    /**
+     * The player and map
+     */
     private PlayerModel playerModel;
     private PlayerView playerView;
     private PlayerController playerController;
@@ -26,7 +32,12 @@ public class GameDoneView extends BasicGameState {
     private MapController mapController;
 
 
-
+    /**
+     * Initializes the player and map
+     * @param gameContainer - The container that has the game
+     * @param stateBasedGame - The current state of the game used to isolate the game from different aspects
+     * @throws SlickException - if the file for the maps is not found, slick-exception.
+     */
     @Override
     public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
         collisionChecker = new CollisionChecker();
@@ -43,6 +54,13 @@ public class GameDoneView extends BasicGameState {
 
     }
 
+    /**
+     * Draws the player and the map on the screen
+     * @param gc - The container that has the game
+     * @param sbg - The current state of the game used to isolate the game from different aspects
+     * @param g - represents the graphics context to be used for rendering.
+     * @throws SlickException - if the file for the maps is not found, slick-exception.
+     */
     @Override
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
         mapView.render(mapModel);
@@ -50,12 +68,22 @@ public class GameDoneView extends BasicGameState {
         mapView.renderTopLayer(mapModel);
     }
 
+    /**
+     * Updates the player and map
+     * @param gc - The container that has the game
+     * @param sbg - The current state of the game used to isolate the game from different aspects
+     * @param delta - represents time in ms since last update.
+     * @throws SlickException - if the file for the maps is not found, slick-exception.
+     */
     @Override
     public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
         playerController.update(gc, sbg, delta);
         mapController.update(gc, delta);
     }
 
+    /** A special ID for this specific state.
+     * @return represents this state with a unique int.
+     */
     @Override
     public int getID() {
         return 100;
