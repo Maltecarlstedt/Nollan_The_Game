949980d4ef8dd76dc22be90b777ff212e2e6dd5a
steffi0929 <steffi.gumman@hotmail.com>
2021-10-19 09:26:13 +0200

    tried doing tests but slick2d problems, fixed the last map also

---
+++ test/java/TestGatheringPantModel.java
@@ -0,0 +1,16 @@
+import Tasks.taskModel.GatheringPantModel;
+import org.junit.jupiter.api.Test;
+import org.newdawn.slick.SlickException;
+
+public class TestGatheringPantModel {
+
+/*
+    @Test
+    public void testGetScore() throws SlickException {
+        GatheringPantModel testScore = new GatheringPantModel();
+
+        testScore.increaseScore();
+        assertTrue()
+    }
+*/
+}
