75c404dd4bf986f2d6cc6741b8224484e9b7dff0
ClaraSimonsson <clara.simonsson@hotmail.se>
2021-10-20 10:58:46 +0200

    Changed if-statements to a switch in the EnterTask class

--- src/main/java/EnterTask.java
+++ src/main/java/EnterTask.java
@@ -8,36 +8,37 @@ import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.state.transition.*;
 
 /** Represents the entering to a task-state
+ *
+ * @author Clara & Malte
  */
 public class EnterTask {
 
     private Input input;
     private String currMap;
 
-    // TODO: fix so the maps are being loaded.
-
     /** Checks the terms in the if-statements  is fulfilled. If it is fulfilled, we jump to the right state.
      * @param gc represents the container that have the game.
      * @param mapModel represents the map in the game.
      * @param sbg the current state of the game used to isolate the game from different aspects.
      */
-
-
-    // NOT AT ALL FINAL. JUST FOR DEV AND TESTING.
     public void update(GameContainer gc, MapModel mapModel, StateBasedGame sbg){
         input = gc.getInput();
         currMap = mapModel.getCurrentMap().toString();
-        if(input.isKeyDown(Input.KEY_F)){
+        if(input.isKeyDown(Input.KEY_F) && mapModel.hasTask()){
             mapModel.taskDone = true;
-            if (currMap.equals("EKAK")){
-                sbg.enterState(69, new FadeInTransition(), new HorizontalSplitTransition()); // Idk ser lite dumt ut
+            int id = sbg.getCurrentStateID();
+            switch (currMap){
+                case "EKAK":
+                    id = 69;
+                    break;
+                case "DELTAP":
+                    id = 420;
+                    break;
+                case "ADAMMEN":
+                    id = 21;
+                    break;
             }
-            else if (currMap.equals("DELTAP")){
-                sbg.enterState(420, new EmptyTransition(), new VerticalSplitTransition());
-            }
-            else if (currMap.equals("ADAMMEN")){
-                sbg.enterState(21, new FadeOutTransition(), new VerticalSplitTransition());
-            }
+            sbg.enterState(id, new FadeInTransition(), new HorizontalSplitTransition());
         }
     }
 }
