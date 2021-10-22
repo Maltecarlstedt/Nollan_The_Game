4f3db724ff88f049b1c9002ab84d5e7bb79738d1
ClaraSimonsson <clara.simonsson@hotmail.se>
2021-10-22 09:33:11 +0200

    Made the model smarter and the controller thinner, refactored some code

--- src/main/java/Items/ItemController.java
+++ src/main/java/Items/ItemController.java
@@ -2,11 +2,6 @@ package Items;
 
 import model.PlayerModel;
 import org.newdawn.slick.state.StateBasedGame;
-import org.newdawn.slick.state.transition.FadeInTransition;
-import org.newdawn.slick.state.transition.FadeOutTransition;
-
-import java.util.Iterator;
-import java.util.Map;
 
     /**
      * The class that dictates the update of the items
@@ -39,65 +34,7 @@ public class ItemController {
          * @param sbg - the canvas
          */
     public void update(PlayerModel pm, ItemModel im, StateBasedGame sbg) {
-        deleteFoundItem(pm, im);
-        gameDone(sbg);
+        im.deleteFoundItem(pm, iv);
+        im.gameDone(sbg);
     }
-
-        /**
-         * Checks if the player has collected all items and so on won the game.
-         * @param sbg - the canvas which to be changed
-         */
-    private void gameDone(StateBasedGame sbg){
-        if (im.itemsCollected()){
-            sbg.enterState(100, new FadeOutTransition(), new FadeInTransition());
-        }
-    }
-
-        /**
-         * Deletes the item when the player
-         * has either intersected with it on the map or won a task.
-         * @param pm - the player
-         * @param im - the items
-         */
-    private void deleteFoundItem(PlayerModel pm, ItemModel im){
-        for (Map.Entry<String, Item> material : im.getItemsToFind().entrySet()) {
-            String key = material.getKey();
-            Item value = material.getValue();
-            if (itemFound(pm, iv, value)) {
-                replace(key, value);
-                break;
-            }
-        }
-    }
-
-        /**
-         * Checks if the player has either intersected with the item on the map
-         * or completed a task on the map.
-         * @param pm - the player
-         * @param iv - our view of the item
-         * @param item - the specific item in our list
-         * @return true if the criteria are achieved
-         */
-    private boolean itemFound(PlayerModel pm, ItemView iv, Item item){
-        return (pm.getNextLocation().intersects(item.location) && iv.currentMap.equals(item.getCurrentMap()));
-    }
-
-        /**
-         * Replaces an item that isn´t found with the collected item.
-         * @param key - The name of the item that are found
-         * @param value - the item with a location and image
-         */
-    private void replace(String key, Item value){
-        for (Map.Entry<String, Item> material : im.getItemsUf().entrySet()) {
-            String replaceableKey = material.getKey();
-            Item replaceableValue = material.getValue();
-            if (replaceableKey.contains(key)) {
-                value.location = replaceableValue.location;
-                im.getItemsUf().replace(replaceableKey, replaceableValue, value);
-                im.getItemsToFind().remove(key, value);
-                break;
-            }
-        }
-    }
-
 }
--- src/main/java/Items/ItemModel.java
+++ src/main/java/Items/ItemModel.java
@@ -2,7 +2,13 @@ package Items;
 
 import Items.Filled.*;
 import Items.Unfilled.*;
+import model.PlayerModel;
+import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.state.transition.FadeInTransition;
+import org.newdawn.slick.state.transition.FadeOutTransition;
+
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Holds the information for the items to be found and the unfilled items
@@ -39,11 +45,67 @@ public class ItemModel {
     HashMap<String, Item> getItemsUf(){ return materialsUnfilled;}
 
     /**
+     * Checks if the player has collected all items and so on won the game.
+     * @param sbg - the canvas which to be changed
+     */
+    void gameDone(StateBasedGame sbg){
+        if (itemsCollected()){
+            sbg.enterState(100, new FadeOutTransition(), new FadeInTransition());
+        }
+    }
+    
+    /**
      * Checks if all items has been found
      * @return true if the list of items to be found is empty
      */
     boolean itemsCollected(){
         return getItemsToFind().size() == 0;
+    }
+
+    /**
+     * Deletes the item when the player
+     * has either intersected with it on the map or won a task.
+     * @param pm - the player
+     * @param iv - our view of the item
+     */
+    void deleteFoundItem(PlayerModel pm, ItemView iv){
+        for (Map.Entry<String, Item> material : getItemsToFind().entrySet()) {
+            String key = material.getKey();
+            Item value = material.getValue();
+            if (itemFound(pm, iv, value)) {
+                replace(key, value);
+                break;
+            }
+        }
+    }
+    /**
+     * Checks if the player has either intersected with the item on the map
+     * or completed a task on the map.
+     * @param pm - the player
+     * @param iv - our view of the item
+     * @param item - the specific item in our list
+     * @return true if the criteria are achieved
+     */
+    private boolean itemFound(PlayerModel pm, ItemView iv, Item item){
+        return (pm.getNextLocation().intersects(item.location) && iv.currentMap.equals(item.getCurrentMap()));
+    }
+
+    /**
+     * Replaces an item that isn´t found with the collected item.
+     * @param key - The name of the item that are found
+     * @param value - the item with a location and image
+     */
+    private void replace(String key, Item value){
+        for (Map.Entry<String, Item> material : getItemsUf().entrySet()) {
+            String replaceableKey = material.getKey();
+            Item replaceableValue = material.getValue();
+            if (replaceableKey.contains(key)) {
+                value.location = replaceableValue.location;
+                getItemsUf().replace(replaceableKey, replaceableValue, value);
+                getItemsToFind().remove(key, value);
+                break;
+            }
+        }
     }
 
     /**
